32617a6b7b120c40931ad77c60903c2645ebe14a
robert-laws <roblaws@me.com>
2018-02-24 16:58:52 +0300

    setup basic session create/destroy functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/home', to: 'static_pages#home', as: 'home'
 
+  # sessions routes
+  get '/signin', to: 'sessions#new'
+  post '/sessions/create', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
 
   # user routes
   get '/users', to: 'users#index'
