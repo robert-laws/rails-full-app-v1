1a05531b97ebb357d0e298683dffb8e7edc3e62e
Robert Laws <roblaws@me.com>
2018-02-24 14:48:26 +0300

    add current_user before_action in application_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  def index
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -10,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user), notice: "You have successfully registered."
     else
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # root route
   root 'static_pages#home'
+  get '/home', to: 'static_pages#home', as: 'home'
+
 
   # user routes
-  resources :users, only: [:show, :new, :create]
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/signup', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
+  
 end
