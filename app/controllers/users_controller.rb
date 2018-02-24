34a3f4c6c5932c6124e44b45a878c56004afa192
robert-laws <roblaws@me.com>
2018-02-24 12:59:04 +0300

    initial setup of user controller, form view, model, and routes for for signup process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,24 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user), notice: "You have successfully registered."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :experience, :credits, :admin)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   # user routes
-  resources :users, only: [:new, :create]
+  resources :users, only: [:show, :new, :create]
 end
