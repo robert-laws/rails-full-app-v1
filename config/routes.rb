8d54c4e3d8d7de02093fabe3242c8684b4427cac
Robert Laws <roblaws@me.com>
2018-02-26 16:32:10 +0300

    clean up spacing

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,4 @@ Rails.application.routes.draw do
   post '/users', to: 'users#create'
   get '/users/:id', to: 'users#show', as: 'user'
   get '/signup', to: 'users#new', as: 'new_user'
-  
-  
 end
